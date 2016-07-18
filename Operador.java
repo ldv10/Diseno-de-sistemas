6c14de5a0e710c922c3610f5007e8ddf65dfb232
JuanCarlosTapia <jc_tapia95@hotmail.com>
2016-07-17 18:25:04 -0600

    Create Operador.java

---
+++ Operador.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Operador {
+	private ArrayList<Juguete> listaJuguetes = new ArrayList<Juguete>();
+	private ArrayList<Proveedor> listaProveedores = new ArrayList<Proveedor>();
+	
+	
+	
+}
